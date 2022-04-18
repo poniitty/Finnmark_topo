4516fc5f66de3d1aee1987bfdae571452bfc68ba
poniitty <pekka.niittynen@helsinki.fi>
2022-04-18 13:38:20 +0300

    aoi created

--- scripts/study_locations.R
+++ scripts/study_locations.R
@@ -5,4 +5,11 @@ d <- read_delim("data/transect_UTM33_start.txt", delim = "\t")
 
 d <- d %>% st_as_sf(coords = c("Longitude","Latitude"), crs = 32633)
 
-d %>% st_write("data/transect_UTM33_start.gpkg")
+# d %>% st_write("data/transect_UTM33_start.gpkg")
+
+bb <- d %>% st_bbox() + c(-10000,-10000,10000,1000)
+aoi <- bb %>% 
+  st_as_sfc() %>% 
+  st_as_sf()
+
+aoi %>% st_write("data/aoi.gpkg")
